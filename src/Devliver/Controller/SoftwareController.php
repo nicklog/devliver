fde83c0eeb8559a630c0b8ce64014b2e746c655e
Nikita Loges <git@nikita-loges.de>
2018-05-02 17:18:50 +0200

    software list

---
+++ src/Devliver/Controller/SoftwareController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Shapecode\Devliver\Controller;
+
+use Composer\Package\CompletePackageInterface;
+use Composer\Package\Loader\ArrayLoader;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class SoftwareController
+ *
+ * @package Shapecode\Devliver\Controller
+ * @author  Nikita Loges
+ *
+ * @Route("/software", name="devliver_software_")
+ */
+class SoftwareController extends Controller
+{
+
+    /**
+     * @Route("/", name="index")
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        $kernel = $this->get('kernel');
+        $projectDir = $kernel->getProjectDir();
+        $lock = $projectDir . '/composer.lock';
+
+        $json = file_get_contents($lock);
+        $data = json_decode($json, true);
+
+        /** @var CompletePackageInterface[] $packages */
+        $packages = [];
+
+        $loader = new ArrayLoader();
+
+        foreach ($data['packages'] as $p) {
+            $package = $loader->load($p);
+
+            $packages[] = $package;
+        }
+
+        return $this->render('@Devliver/Software/index.html.twig', [
+            'packages' => $packages
+        ]);
+    }
+}
